3b4036adb20485748e66ce3d7b471584e7ea943b
foxmessenger <iamliamfox@gmail.com>
2017-04-29 15:56:54 -0500

    added dependencies

---
+++ rps-game/server.js
@@ -0,0 +1,32 @@
+var mysql 		= require('mysql');
+var express 	= require('express');
+var bodyParser 	= require('body-parser');
+var exphbs 		= require("express-handlebars");
+var app 		= express();
+var path 		= require('path');
+var PORT 		= process.env.PORT || 7002;
+
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// app.use(express.static('./public'));
+
+// Service 2 (Game Service)
+
+// 1. check to make sure sessionid exists in users table
+// 2. check to make sure friendname exists in users table
+// 3. get second sessionid from users table using friendname
+// 4. generate uid(16) for gameid
+// 5. put both sessionids and gameid into games table
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "wishes_db"
+});
+
+
+app.listen(PORT, function() {
+	console.log('App listening on PORT ' + PORT);
+});
